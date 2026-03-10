3a6c631937e0a2176b1dec458b5df5a096b7c892
DAVID ALIENYI <davidalienyi@Davids-MacBook-Pro.local>
2026-03-10 01:35:12 +0100

    fix

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -49,7 +49,7 @@ import {
       @MessageBody() dto: any,
       @ConnectedSocket() client: Socket,
     ) {
-      console.log(client.data, 'Client Data.....');
+      console.log(client.data, 'Client Data.....', dto);
       const user = client.data.id; // from JWT guard
       const email = await this.chatService.decrypt(user);
       console.log(email, 'Email.........');
